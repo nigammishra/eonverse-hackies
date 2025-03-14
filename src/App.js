8e4045a5025b368c91a3a3f1d5105ee2dc8f7079
nigam <nigammishra826@gmail.com>
2025-03-14 13:12:31 +0530

    deployment complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './headandfoot/Header.jsx'
+import Hero from './component/Hero.jsx'
+import Main from './component/MainPage.jsx'
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<Hero />} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
